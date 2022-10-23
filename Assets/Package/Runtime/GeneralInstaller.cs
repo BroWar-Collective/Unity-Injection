0432945151829a88c3a8aea9fcccb7965f571225
Miłosz Matkowski <milosz.matkowski96@gmail.com>
2022-10-24 00:10:13 +0200

    Add Zenject to the project

---
+++ Assets/Package/Runtime/GeneralInstaller.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using Zenject;
+
+namespace BroWar.Injection
+{
+    [AddComponentMenu("BroWar/Injection/General Installer")]
+    public class GeneralInstaller : MonoInstaller<GeneralInstaller>
+    {
+        [SerializeReference, ReferencePicker(TypeGrouping = TypeGrouping.ByFlatName), ReorderableList(elementLabel: "Installer")]
+        private IInstaller[] installers;
+
+        public override void InstallBindings()
+        {
+            if (installers == null)
+            {
+                return;
+            }
+
+            for (var i = 0; i < installers.Length; i++)
+            {
+                installers[i].Install(Container);
+            }
+        }
+    }
+}
