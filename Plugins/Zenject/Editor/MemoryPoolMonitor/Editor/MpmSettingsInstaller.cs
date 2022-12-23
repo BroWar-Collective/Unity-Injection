3281b244b5a6741b45c17a2f563f711a823af639
Miłosz Matkowski <milosz.matkowski96@gmail.com>
2022-12-23 21:58:21 +0100

    Zenject update; possibility to setup predefined ProjectContext

---
+++ Plugins/Zenject/Editor/MemoryPoolMonitor/Editor/MpmSettingsInstaller.cs
@@ -0,0 +1,15 @@
+﻿using UnityEditor;
+
+namespace Zenject.MemoryPoolMonitor
+{
+    public class MpmSettingsInstaller : ScriptableObjectInstaller<MpmSettingsInstaller>
+    {
+        public MpmView.Settings MpmView;
+        public MpmView.Settings MpmViewDark;
+
+        public override void InstallBindings()
+        {
+            Container.BindInstance(EditorGUIUtility.isProSkin ? MpmViewDark : MpmView);
+        }
+    }
+}
---
+++ Runtime/IInjectionManager.cs
@@ -0,0 +1,12 @@
+﻿using Zenject;
+
+namespace BroWar.Injection
+{
+    public interface IInjectionManager
+    {
+        /// <summary>
+        /// <see cref="ProjectContext"/> used during injection systems initialization.
+        /// </summary>
+        ProjectContext ProjectContextPrefab { get; set; }
+    }
+}
