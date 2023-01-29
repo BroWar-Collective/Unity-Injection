774b237cfba0c42ab0251fdf0edf4a645884fefb
Miłosz Matkowski <milosz.matkowski96@gmail.com>
2023-01-30 00:53:26 +0100

    API for processable installers & minor refactor changes

---
+++ Assets/Package/Runtime/ContextInstaller.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using Zenject;
+
+namespace BroWar.Injection
+{
+    using BroWar.Common;
+
+    [AddComponentMenu("BroWar/Injection/Context Installer")]
+    public class ContextInstaller : MonoInstaller<ContextInstaller>
+    {
+        [SerializeReference, ReferencePicker(TypeGrouping = TypeGrouping.ByFlatName), ReorderableList(elementLabel: "Installer")]
+        private ISubInstaller[] installers;
+
+        protected virtual void HandleInstallers()
+        {
+            var count = installers?.Length ?? 0;
+            for (var i = 0; i < count; i++)
+            {
+                var installer = installers[i];
+                if (installer == null)
+                {
+                    LogHandler.Log($"[Injection][{name}] Installer at '{i}' is invalid.", LogType.Warning);
+                    continue;
+                }
+
+                installer.Install(Container);
+            }
+        }
+
+        public override void InstallBindings()
+        {
+            HandleInstallers();
+        }
+
+        public IReadOnlyList<ISubInstaller> Installers => installers;
+    }
+}
---
+++ Assets/Package/Runtime/ExposableInstaller.cs
@@ -0,0 +1,72 @@
+﻿using UnityEngine;
+using Zenject;
+
+namespace BroWar.Injection
+{
+    using BroWar.Common;
+
+    /// <summary>
+    /// Installer that can be exposed to a parent context, e.g. from <see cref="GameObjectContext"/> to <see cref="SceneContext"/>.
+    /// </summary>
+    public abstract class ExposableInstaller : ISubInstaller
+    {
+        [SerializeField, Tooltip("If true parent container will be used for binding." +
+            " This is useful when we want to expose references outside our context.")]
+        private bool bindToParent;
+
+        private DiContainer GetParentContainer(DiContainer target)
+        {
+            var parents = target.ParentContainers;
+            if (parents == null || parents.Length == 0)
+            {
+                return null;
+            }
+
+            return parents[0];
+        }
+
+        protected abstract void OnInstall(DiContainer container);
+
+        public void Install(DiContainer container)
+        {
+            TargetContainer = container;
+            ParentContainer = GetParentContainer(container);
+            if (BindToParent)
+            {
+                if (ParentContainer != null)
+                {
+                    container = ParentContainer;
+                }
+                else
+                {
+                    LogHandler.Log("[Game] Cannot find parent container.", LogType.Warning);
+                }
+            }
+
+            OnInstall(container);
+        }
+
+        protected bool BindToParent
+        {
+            get => bindToParent;
+            set => bindToParent = value;
+        }
+
+        /// <summary>
+        /// Default <see cref="DiContainer"/> associated to "our" context.
+        /// </summary>
+        protected DiContainer TargetContainer
+        {
+            get; private set;
+        }
+
+        /// <summary>
+        /// Parent <see cref="DiContainer"/> associated with higher context 
+        /// (e.g. <see cref="SceneContext"/> if installer is placed within <see cref="GameObjectContext"/>).
+        /// </summary>
+        protected DiContainer ParentContainer
+        {
+            get; private set;
+        }
+    }
+}
