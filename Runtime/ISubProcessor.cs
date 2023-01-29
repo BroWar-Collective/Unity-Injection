7df6d14f2612f1c9656ac79867380424f80c1d90
Miłosz Matkowski <milosz.matkowski96@gmail.com>
2023-01-30 00:53:26 +0100

    API for processable installers & minor refactor changes

---
+++ Runtime/ISubProcessor.cs
@@ -0,0 +1,16 @@
+﻿namespace BroWar.Injection
+{
+    public interface ISubProcessor<T>
+    {
+        /// <summary>
+        /// Called right before the installation process.
+        /// </summary>
+        /// <param name="data">Data used to initialize the context installer.</param>
+        void OnBeginInstallation(T data);
+        /// <summary>
+        /// Called right after the installation process.
+        /// </summary>
+        /// <param name="data">Data used to initialize the context installer.</param>
+        void OnCloseInstallation(T data);
+    }
+}
---
+++ Runtime/ProcessableInstaller.cs
@@ -0,0 +1,69 @@
+﻿using BroWar.Common;
+using UnityEngine;
+
+namespace BroWar.Injection
+{
+    public abstract class ProcessableInstaller<T0, T1> : ContextInstaller where T1 : ISubProcessor<T0>
+    {
+        [Tooltip("We can use custom processors to run custom logic before and after the installation.")]
+        [SerializeReference, ReferencePicker(TypeGrouping = TypeGrouping.ByFlatName), ReorderableList(elementLabel: "Processor")]
+        protected T1[] processors;
+
+        protected virtual void BeginInstallation(T0 data)
+        {
+            if (processors == null)
+            {
+                return;
+            }
+
+            for (var i = 0; i < processors.Length; i++)
+            {
+                var processor = processors[i];
+                if (processor == null)
+                {
+                    LogHandler.Log($"[Injection][{name}] Processor at '{i}' is invalid.", LogType.Warning);
+                    continue;
+                }
+
+                BeginInstallation(data, processor);
+            }
+        }
+
+        protected virtual void CloseInstallation(T0 data)
+        {
+            if (processors == null)
+            {
+                return;
+            }
+
+            for (var i = 0; i < processors.Length; i++)
+            {
+                var processor = processors[i];
+                if (processor == null)
+                {
+                    LogHandler.Log($"[Injection][{name}] Processor at '{i}' is invalid.", LogType.Warning);
+                    continue;
+                }
+
+                CloseInstallation(data, processor);
+            }
+        }
+
+        protected virtual void InstallBindings(T0 data)
+        {
+            BeginInstallation(data);
+            base.InstallBindings();
+            CloseInstallation(data);
+        }
+
+        protected abstract void BeginInstallation(T0 data, T1 processor);
+        protected abstract void CloseInstallation(T0 data, T1 processor);
+        protected abstract T0 GetProcessingData();
+
+        public sealed override void InstallBindings()
+        {
+            var data = GetProcessingData();
+            InstallBindings(data);
+        }
+    }
+}
